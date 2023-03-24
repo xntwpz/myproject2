140fed4e267c201656a6497751515b7c45b6c5c7
xntwpz <nattawank51@gmail.com>
2023-03-24 21:58:41 +0700

    version1.2

---
+++ MyProject2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myproject2;
+
+/**
+ *
+ * @author N192
+ */
+public class MyProject2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        // TODO code application logic here
+    }
+    
+}
