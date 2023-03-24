743417f082b32981ec1222a32db1858e55e864cb
xntwpz <nattawank51@gmail.com>
2023-03-24 22:00:42 +0700

    version 2

--- MyProject2.java
+++ MyProject2.java
@@ -15,6 +15,7 @@ public class MyProject2 {
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Nattawan");
         // TODO code application logic here
     }
     
